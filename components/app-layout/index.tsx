89f0a5eb5590bd51e35a8cda08cd46626987a5bf
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-09 01:09:32 +0300

    feat: project base

---
+++ components/app-layout/index.tsx
@@ -0,0 +1,39 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Image from 'next/image'
+import { signOut } from "next-auth/react"
+
+
+type LayoutProps = {
+    children: React.ReactNode,
+};
+export default function Layout({ children }: LayoutProps) {
+    const { data } = useSession()
+
+    return (
+        <>
+            <div className="min-h-screen flex flex-col">
+                <header className="flex justify-between p-2">
+                    <nav>
+                    </nav>
+                    {data && (
+                        <>
+                            <div className="flex items-center gap-x-2">
+                                <p>{data.user?.name}</p>
+                                <Image alt={data.user?.name || ''} src={data.user?.image || ''} width={48} height={48} className="rounded-full" />
+                            </div>
+                            <button onClick={() => signOut()} className="border rounded-md px-4">Sign out</button>
+                        </>
+                    )
+                    }
+                </header>
+                <main className="flex grow px-10">
+                    {children}
+                </main>
+            </div>
+            <footer className="flex py-8 border-t border-gray-400 items-center justify-center mx-3">
+                Powered by kbq
+            </footer>
+        </>
+    )
+}
