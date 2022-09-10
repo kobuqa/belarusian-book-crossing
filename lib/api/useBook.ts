a4f57afb5c57a4d56f326eb090dc88c25d42d38c
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-10 21:34:03 +0300

    fix: port removing

--- lib/api/useBook.ts
+++ lib/api/useBook.ts
@@ -7,7 +7,7 @@ export const useBook = () => {
   const router = useRouter();
 
   const addBook = async (book: BookDto) => {
-    const update = await axios.post("https://104.248.94.208:8181/api/books", book);
+    const update = await axios.post("https://104.248.94.208/api/books", book);
     if (update.status == 200) router.replace(router.asPath);
   };
 
