01c859a565732661cbc6c9cf616562d69ba82b78
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-10 17:18:48 +0300

    fix: fixed layout

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,21 +1,17 @@
-//@ts-nocheck
 import Head from 'next/head'
-import React, { useEffect, useState } from 'react'
 import Layout from '../components/app-layout'
+import Image from 'next/image';
 
-const logo = require('https://i.postimg.cc/ydTFQQRn/BBC.png');
-
+export default function About() {
   return (
     <Layout>
       <Head>
         <title>Belarusian Book Crossing</title>
       </Head>
       <div className="page-content">
-        <div className="logo-pic">
-          <img src={String(logo)} />
-        </div>
+          <Image src="/logo.png" width={200} height={100} alt="logo" />
         <div>
-          <label htmlFor="fid-1" className="peer-checked:bg-gray-300 p-2 cursor-pointer w-28">Вас вітае Belarusian Book Crissing!</label>
+          <label htmlFor="fid-1" className="peer-checked:bg-gray-300 p-2 cursor-pointer w-28">Вас вітае Belarusian Book Crossing!</label>
           <label htmlFor="fid-1" className="peer-checked:bg-gray-300 p-2 cursor-pointer w-28">Мы праэкт, прызначаны для таго, каб дапамагчы табе знайсці беларускія кніжкі і аднадумцаў за мяжой.</label>
           <label htmlFor="fid-1" className="peer-checked:bg-gray-300 p-2 cursor-pointer w-28">Уводзь назву твора ці нумар ISBN праз форму пошуку і атрымай спіс лакацый, дзе нехта гатовы падзяліцца кніжкай.</label>
           <label htmlFor="fid-1" className="peer-checked:bg-gray-300 p-2 cursor-pointer w-28">Як гэта працуе?</label>
@@ -28,7 +24,3 @@ const logo = require('https://i.postimg.cc/ydTFQQRn/BBC.png');
     </Layout>
   )
 }
-
-export async function getServerSideProps() {
-}
-export default Home
