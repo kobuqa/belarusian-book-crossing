92665fe625a608d325350cc7ec2264968785ab6e
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-11 12:19:57 +0300

    fix: add active link styles

--- components/app-layout/index.tsx
+++ components/app-layout/index.tsx
@@ -16,9 +16,9 @@ export default function Layout({ children }: LayoutProps) {
             <div className="min-h-screen flex flex-col">
                 <header className="flex justify-between p-2">
                     <nav className="flex">
-                        <li className="bg-gray-200 hover:bg-gray-300 p-3 border-r border-r-gray-300 cursor-pointer" onClick={() => router.push('/')}>Галоўная</li>
-                        <li className="bg-gray-200 hover:bg-gray-300 p-3 border-r border-r-gray-300 cursor-pointer" onClick={() => router.push('/map')}>Мапа</li>
-                        <li className="bg-gray-200 hover:bg-gray-300 p-3 cursor-pointer" onClick={() => router.push('/about')}>Пра нас</li>
+                        <li className={`bg-gray-200 hover:bg-gray-300 p-3 border-r border-r-gray-300 cursor-pointer ${router.pathname === '/' && 'bg-gray-300'}`} onClick={() => router.push('/')}>Галоўная</li>
+                        <li className={`bg-gray-200 hover:bg-gray-300 p-3 border-r border-r-gray-300 cursor-pointer ${router.pathname === '/map' && 'bg-gray-300'}`} onClick={() => router.push('/map')}>Мапа</li>
+                        <li className={`bg-gray-200 hover:bg-gray-300 p-3  cursor-pointer ${router.pathname === '/about' && 'bg-gray-300'}`} onClick={() => router.push('/about')}>Пра нас</li>
                     </nav>
                     {data && (
                         <>
