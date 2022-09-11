f9b1103b37f6f5e55180d821f307f1a32db7c395
Alexander Matveichikov <alexander.matveichikov@mbicycle.com>
2022-09-11 12:41:28 +0300

    fix: added about us

--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,28 @@ export default function About() {
       <Head>
         <title>Belarusian Book Crossing</title>
       </Head>
-     
+      <div className="p-4">
+        <ul>
+          <li>
+            Ганна - ідэя, арганізацыя
+          </li>
+          <li>
+            Аляксандр - frontend developer
+          </li>
+          <li>
+            Аляксей - backend developer
+          </li>
+          <li>
+            Аляксандра - універсальны салдат
+          </li>
+          <li>
+            Кірыл - распрацоўка бота
+          </li>
+          <li>
+            Піліп - распрацоўка бота
+          </li>
+        </ul>
+      </div>
     </Layout>
   )
 }
